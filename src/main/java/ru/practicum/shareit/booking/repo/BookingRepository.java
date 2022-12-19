436976da5cd46d831cff4b96dfbd088503d89020
IvanKondrashkov <vanya.kondrashkov.92@mail.ru>
2022-12-19 19:23:01 +0500

    style: fixed code style

--- src/main/java/ru/practicum/shareit/booking/repo/BookingRepository.java
+++ src/main/java/ru/practicum/shareit/booking/repo/BookingRepository.java
@@ -11,8 +11,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface BookingRepository extends JpaRepository<Booking, Long> {
     @Query("select b from Booking b where b.booker.id = ?1")
     List<Booking> findAllByBookerId(Long userId, Sort sort);
+
     @Query("select b from Booking b inner join Item i on b.item.id = i.id where i.owner.id = ?1")
     List<Booking> findAllByOwnerId(Long userId, Sort sort);
+
     @Query("select b from Booking b inner join Item i on b.item.id = ?1")
     List<Booking> findAllByItemId(Long itemId);
 }
--- src/main/java/ru/practicum/shareit/item/repo/ItemRepository.java
+++ src/main/java/ru/practicum/shareit/item/repo/ItemRepository.java
@@ -9,6 +9,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
     List<Item> findAllByOwnerId(Long userId);
+
     @Query("select i from Item i where i.available = true and (lower(i.name) like lower(concat('%', ?1, '%')) or lower(i.description) like lower(concat('%', ?1, '%')))")
     List<Item> findAllByText(String text);
 }
